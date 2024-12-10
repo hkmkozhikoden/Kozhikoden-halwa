9d8ae597368d89041e65d4ef97874dded92a1167
hkmkozhikoden <hkmkozhikoden@gmail.com>
2024-12-10 19:43:04 +0530

    slider responsive setting

--- src/component/testimonial.jsx
+++ src/component/testimonial.jsx
@@ -44,6 +44,24 @@ function Testimonial() {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+        ]
     };
 
     return (
